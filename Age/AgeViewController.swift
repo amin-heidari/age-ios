444bd8a03719f0e898352939e109117c4ca974c5
Amin <dev@aminheidari.com>
2019-05-23 17:29:01 -0400

    Initiated the navigation scheme.

---
+++ Age/AgeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AgeViewController.swift
+//  Age
+//
+//  Created by Amin on 2019-05-23.
+//  Copyright © 2019 Amin Heidari. All rights reserved.
+//
+
+import UIKit
+
+class AgeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
