881f595c718af5ea8192f28a5d66a4ce9a237d20
Amin Heidari <dev@aminheidari.com>
2019-05-25 19:44:34 -0400

    Initiated for dev environment.

---
+++ Age/Constants/Dev/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Age
+//
+//  Created by Amin on 2019-05-19.
+//  Copyright © 2019 Amin Heidari. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    class RemoteConfig {
+        static let URL = "https://dev.api.config.aminheidari.com/projects/agepal/ios"
+        static let FreshCacheTime: TimeInterval = 30 // Time interval (in seconds) during which the cache will be used rather than making a new api call.
+        static let ExpireTime: TimeInterval = 1 * 24 * 3600 // Time interval (in seconds) after which the cache expires and a fresh remote config MUST be fetched.
+    }
+    
+}
