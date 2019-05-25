6762f73505d44aa9a6bcf9e4e73694e93c9ecf60
Amin Heidari <dev@aminheidari.com>
2019-05-25 16:31:01 -0400

    Initiated root view controller.

---
+++ Age/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootViewController.swift
+//  Age
+//
+//  Created by Amin on 2019-05-25.
+//  Copyright © 2019 Amin Heidari. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
