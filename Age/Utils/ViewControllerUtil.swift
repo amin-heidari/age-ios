741d3d3f3a3d2dc063dd6cf704c07b713bd5da63
Amin <dev@aminheidari.com>
2019-06-03 17:57:55 -0400

    AGE-14 Added missing files.

---
+++ Age/Utils/ViewControllerUtil.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerUtil.swift
+//  Age
+//
+//  Created by Amin on 2019-06-03.
+//  Copyright © 2019 Amin Heidari. All rights reserved.
+//
+
+import Foundation
+
+//extension UIViewController {
+//    
+//}
---
+++ Age/ViewControllers/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  Age
+//
+//  Created by Amin on 2019-06-03.
+//  Copyright © 2019 Amin Heidari. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    // MARK: - Constants/Types
+    
+    // MARK: - Static
+    
+    // MARK: - API
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+
+}
