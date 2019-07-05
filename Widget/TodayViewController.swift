df0f6ef83bc69ee9785026bc1dae7ce7a9150806
Amin Heidari <dev@aminheidari.com>
2019-07-05 03:50:00 -0400

    AGE-47 Logic wired.

--- Widget/TodayViewController.swift
+++ Widget/TodayViewController.swift
@@ -11,23 +11,87 @@ import NotificationCenter
 import AgeData
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    
+    // MARK: - Constants/Types
+    
+    // MARK: - Static
+    
+    // MARK: - API
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Setup the SuiteDefaultsUtil
         SuiteDefaultsUtil.suiteName = Constants.UserDefaults.suiteName
+        
+        // No expanded mode support for now.
+        extensionContext?.widgetLargestAvailableDisplayMode = .compact
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Start running the updates.
+        guard let bday = birthday else {
+            timer?.invalidate()
+            timer = nil
+            
+            ageLabel.text = "TBD"
+            
+            return
+        }
+        
+        ageCalculator = AgeCalculator(birthDate: bday.birthDate)
+        
+        timer = Timer.scheduledTimer(timeInterval: Constants.AgeCalculation.refreshInterval,
+                                     target: self,
+                                     selector: #selector(refreshAge),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // Stop running the updates.
+        timer?.invalidate()
+        timer = nil
+        ageCalculator = nil
+        
+        super.viewDidDisappear(animated)
+    }
+    
+    // MARK: - Properties
+    
+    private var birthday: Birthday?
+    private var ageCalculator: AgeCalculator?
+    
+    private var timer: Timer?
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    // MARK: - Methods
+    
+    @objc private func refreshAge() {
+        guard let calculator = ageCalculator, isViewLoaded else {
+            return
+        }
+        
+        ageLabel.text = String(format: "%.8f", calculator.currentAge.value)
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - NCWidgetProviding
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
         
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
+        // Evaluate the calculator.
+        birthday = SuiteDefaultsUtil.defaultBirthday
         
-        completionHandler(NCUpdateResult.newData)
-        
-        
+        completionHandler(.newData)
     }
     
 }
